1854afa65ccca88ceda65a271cc16b0e4dabd326
Aleksandra <aleksandragraf30112510@gmail.com>
2018-06-01 20:56:27 +0700

    Left border of the game window

--- src/game.cpp
+++ src/game.cpp
@@ -21,6 +21,13 @@ Sprite win_sprite;
 Font font;
 Text text("", font, 20);
 
+bool isLeftBorder(double platform_position)
+{
+	if(platform_position < 0)
+		return true;
+	return false;
+}
+
 bool collapse(block bl) // проверка на столкновение шара с блоком
 {
 	if ((ball_x > bl.cx1 && ball_x < bl.cx2) && (ball_y > bl.cy1 && ball_y < bl.cy2))
